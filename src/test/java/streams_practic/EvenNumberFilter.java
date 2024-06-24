fdd3ca83992177a1f7b69f2b496c33ee4a8d0af9
orehg <arsegor5@gmail.com>
2024-06-24 16:54:06 +0300

    deleted evenNumberFilter from streams_practic

--- src/test/java/streams_practic/EvenNumberFilter.java
+++
@@ -1,9 +0,0 @@
-package streams_practic;
-
-import java.util.stream.Stream;
-
-public class EvenNumberFilter {
-    public static void main(String[] args) {
-        Stream.of(1,2,3,4,5,6,7,8,9).filter(element -> element % 2 == 0).forEach(System.out::println);
-    }
-}
